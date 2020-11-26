0ceecfaa1a1f92642580d0b3bfab379041af5498
mmohamed-and <mmohamed@and.digital>
2020-11-26 16:15:22 +0000

    Add header buttons to allow you to toggle between forms

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import AddRestaurantForm from '../components/AddRestaurantForm/AddRestaurantForm';
 import TileBlock from '../components/TileBlock/TileBlock';
 import Button from '../components/atom/Button/Button';
 import Footer from '../components/atom/Footer/Footer';
@@ -6,6 +7,14 @@ import Footer from '../components/atom/Footer/Footer';
 import '../styles/App.css';
 
 function App() {
+	const [update, setUpdate] = useState(false);
+	const handleAdd = (e) => {
+		setUpdate(false)
+	}
+
+	const handleUpdate = (e) => {
+		setUpdate(true);
+	}
 
 
 	return (
@@ -13,6 +22,22 @@ function App() {
 			<h1>Restaurants</h1>
 			<div className="forms">
 				<div className="form-inner">
+					<div className="header-buttons">
+						<Button
+							type="button"
+							id="add"
+							className={update === false ? "add-header-btn button-on" : "add-header-btn"}
+							onClick={handleAdd}
+							buttonLabel="Add"
+						/>
+						<Button
+							type="button"
+							id="update"
+							className={!update === false ? "update-header-btn button-on" : "update-header-btn"}
+							onClick={handleUpdate}
+							buttonLabel="Update"
+						/>
+					</div>
 					<AddRestaurantForm updatedState={update} />
 				</div>
 			</div>
